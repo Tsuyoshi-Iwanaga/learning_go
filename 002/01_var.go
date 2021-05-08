342b10879c3fba9813a78f9e8cc98a646d0d9c9e
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-08 21:15:22 +0900

    go variable

---
+++ 002/01_var.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var a, b, c int = 100, 200, 300
+	// var a, b, c = 100, 200, 300
+	a, b, c := 100, 200, 300
+
+	fmt.Print("total:")
+	fmt.Println(a+b+c)
+}
