cc2336b524b4cfd7d7b1ce6f35623c58528c37bf
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-10 23:42:52 +0900

    go 10_func_arg

---
+++ 002/10_func_arg.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	m := []string {"one", "two", "three"}
+	fmt.Println(m)
+	m = push(m, "1", "2", "3")
+	fmt.Println(m)
+}
+
+//可変長引数
+func push(a []string, v ...string) (s []string) {
+	s = append(a, v...)
+	return
+}
