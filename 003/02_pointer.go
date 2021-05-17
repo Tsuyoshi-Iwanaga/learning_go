d5c1f9794511d7ba450f996115c349527539fac1
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-17 23:36:42 +0900

    go pointer.go

---
+++ 003/02_pointer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	n := 123
+	m := 10000
+
+	p := &n
+	p2 := &m
+	fmt.Printf("p value:%d, address:%p\n", *p, p)
+	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+
+	//ポインタの中身を入れ替える
+	pb := p
+	p = p2
+	p2 = pb
+	fmt.Printf("p value:%d, address:%p\n", *p, p)
+	fmt.Printf("p2 value:%d, address:%p\n", *p2, p2)
+}
