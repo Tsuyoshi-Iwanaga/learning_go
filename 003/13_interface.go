6f393a80f3d632e5a5a2b0512a22175dea6c2048
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-23 14:12:00 +0900

    go interface

---
+++ 003/13_interface.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+//空のインターフェース、どんな値でも入れることができる
+type General interface{}
+
+type GData interface {
+	Set(nm string, g General)
+	Print()
+}
+
+type GDataImpl struct {
+	Name string
+	Data General
+}
+
+func (gd *GDataImpl) Set(nm string, g General) {
+	gd.Name = nm
+	gd.Data = g
+}
+
+func (gd *GDataImpl) Print() {
+	fmt.Printf("<<%s>> ", gd.Name)
+	fmt.Println(gd.Data)
+}
+
+func main() {
+	var data = []GDataImpl{}
+	data = append(data, GDataImpl{"Taro", 123})
+	data = append(data, GDataImpl{"Hanako", "hello"})
+	data = append(data, GDataImpl{"Sachiko", []int{123, 456, 789}})
+	for _, ob := range data {
+		ob.Print()
+	}
+}
