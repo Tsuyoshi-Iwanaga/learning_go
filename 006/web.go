a7982c091de6ba7e4a804faf881d7beb4d0c940d
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-29 22:08:36 +0900

    go web server dynamic

--- 006/web.go
+++ 006/web.go
@@ -5,10 +5,17 @@ import (
 )
 
 func main() {
+	msg := `<html><body>
+		<h1>Hello</h1>
+		<p>This is GO-server</p>
+		</body></html>`
+
 	hh := func(w http.ResponseWriter, rq *http.Request) {
-		w.Write([]byte("Hello, This is GO-server!!"))
+		w.Write([]byte(msg))
 	}
+
 	http.HandleFunc("/hello", hh)
+
 	http.ListenAndServe("127.0.0.1:8050", nil)
 	// http.ListenAndServe("127.0.0.1:8050", http.FileServer(http.Dir(".")))
 }
