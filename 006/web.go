0530f53d1d5db792d2ff760a7c6daf155aa7786d
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-29 21:55:21 +0900

    go web routing

--- 006/web.go
+++ 006/web.go
@@ -5,5 +5,10 @@ import (
 )
 
 func main() {
-	http.ListenAndServe("127.0.0.1:8050", http.FileServer(http.Dir(".")))
+	hh := func(w http.ResponseWriter, rq *http.Request) {
+		w.Write([]byte("Hello, This is GO-server!!"))
+	}
+	http.HandleFunc("/hello", hh)
+	http.ListenAndServe("127.0.0.1:8050", nil)
+	// http.ListenAndServe("127.0.0.1:8050", http.FileServer(http.Dir(".")))
 }
