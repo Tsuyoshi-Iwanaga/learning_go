53f27e85479ed5cc6a84c815048dbf1c330884c1
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-30 13:43:35 +0900

    go web server template

--- 006/web.go
+++ 006/web.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-	msg := `<html><body>
+	html := `<html>
+		<body>
 		<h1>Hello</h1>
-		<p>This is GO-server</p>
+		<p>This is sample message.</p>
 		</body></html>`
+	tf, er := template.New("index").Parse(html)
+	if er != nil {
+		log.Fatal(er)
+	}
 
 	hh := func(w http.ResponseWriter, rq *http.Request) {
-		w.Write([]byte(msg))
+		er = tf.Execute(w, nil)
+		if er != nil {
+			log.Fatal(er)
+		}
 	}
 
 	http.HandleFunc("/hello", hh)
 
 	http.ListenAndServe("127.0.0.1:8050", nil)
-	// http.ListenAndServe("127.0.0.1:8050", http.FileServer(http.Dir(".")))
 }
