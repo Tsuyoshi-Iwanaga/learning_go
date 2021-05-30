bcf836466b2909a1043df36aebcf64bc491fffad
Tsuyoshi-Iwanaga <tsuyo.1991@gmail.com>
2021-05-30 15:33:42 +0900

    update go routing

--- 006/web.go
+++ 006/web.go
@@ -6,20 +6,72 @@ import (
 	"net/http"
 )
 
-func main() {
-	tf, er := template.ParseFiles("templates/hello.html")
+//Temps is template structure
+type Temps struct {
+	notemp *template.Template
+	indx   *template.Template
+	helo   *template.Template
+}
+
+//Template for no-template.
+func notemp() *template.Template {
+	src := "<html><body><h1>NO TEMPLATE.</h1></body></html>"
+	tmp, _ := template.New("index").Parse(src)
+	return tmp
+}
+
+//setup template function
+func setupTemp() *Temps {
+	temps := new(Temps)
+
+	temps.notemp = notemp()
+
+	//set index template
+	indx, er := template.ParseFiles("templates/index.html")
 	if er != nil {
-		tf, _ = template.New("index").Parse("<html><body><h1>NO TEMPLATE.</h1></body></html>")
+		indx = temps.notemp
 	}
+	temps.indx = indx
 
-	hh := func(w http.ResponseWriter, rq *http.Request) {
-		er = tf.Execute(w, nil)
-		if er != nil {
-			log.Fatal(er)
-		}
+	//set hello template.
+	helo, er := template.ParseFiles("templates/hello.html")
+	if er != nil {
+		helo = temps.notemp
 	}
+	temps.helo = helo
 
-	http.HandleFunc("/hello", hh)
+	return temps
+}
+
+//index handler
+func index(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
+	er := tmp.Execute(w, nil)
+	if er != nil {
+		log.Fatal(er)
+	}
+}
+
+//hello handler.
+func hello(w http.ResponseWriter, rq *http.Request, tmp *template.Template) {
+	er := tmp.Execute(w, nil)
+	if er != nil {
+		log.Fatal(er)
+	}
+}
+
+func main() {
+
+	temps := setupTemp()
+
+	//index handling.
+	http.HandleFunc("/", func(w http.ResponseWriter, rq *http.Request) {
+		index(w, rq, temps.indx)
+	})
+
+	//hello handling
+	http.HandleFunc("/hello", func(w http.ResponseWriter, rq *http.Request) {
+		hello(w, rq, temps.helo)
+	})
 
 	http.ListenAndServe("127.0.0.1:8050", nil)
 }
